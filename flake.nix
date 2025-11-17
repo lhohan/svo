fa1cbc1192199cf054068656f3dd2ecab9b291e7
lhohan <2922363+lhohan@users.noreply.github.com>
2025-11-17 11:35:57 +0100

    build: Add dependency mgt: nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Nix flake for Rust project with just support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustup
+      ];
+    };
+  });
+}
